47e570c2c4fa84b33a32e6fff9c05a8476f3ab59
darley <darleychagas03@gmail.com>
2022-08-07 22:38:42 -0400

    Adicionando cliente 1

--- modulo1/src/aula2/ProgramaPrincipalAula2.java
+++ modulo1/src/aula2/ProgramaPrincipalAula2.java
@@ -3,7 +3,19 @@ package aula2;
 public class ProgramaPrincipalAula2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Cliente c1, c2, c3;
+		
+		c1 = new Cliente();
+		c1.nome="João";
+		c1.cpf="001.110.221-10";
+		c1.rg="23299281";
+		c1.peso=70.0; //Double
+		c1.altura=1.70; //Double
+		c1.ativado=true;
+		
+		c2 = new Cliente();
+		c3 = new Cliente();
 
 	}
 
